d3e05e9de1da1adae8460b26e0f4e99413f15d0a
ayrokid <ayrokid@gmail.com>
2015-08-01 21:25:48 +0700

    sync

---
+++ hello/app/controllers/hello_controller.rb
@@ -0,0 +1,10 @@
+class HelloController < ApplicationController
+
+	def home
+		puts "Honey, I'm love you full"
+	end
+
+	def hello
+		puts "Hello"
+	end
+end
---
+++ todo/app/controllers/todo_controller.rb
@@ -0,0 +1,30 @@
+class TodoController < ApplicationController
+	def index
+		@content = 'this content';
+	end
+
+	def new
+
+	end
+
+	def create
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def show
+		
+	end
+
+	def destroy
+		
+	end
+
+end
