7e4682000db733a505a1516b5b20637960af9e38
ayrokid <ayrokid@gmail.com>
2015-09-15 07:56:39 +0700

    app 3

--- app2/db/schema.rb
+++ app2/db/schema.rb
@@ -13,6 +13,14 @@
 
 ActiveRecord::Schema.define(version: 20150908044442) do
 
+  create_table "customers", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "address",    limit: 255
+    t.string   "phone",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.string   "email",      limit: 255
